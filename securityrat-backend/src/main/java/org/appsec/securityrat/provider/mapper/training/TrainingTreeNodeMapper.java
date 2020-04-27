3333a902e0b40067f670a6814545755f56ab0777
Luis Felger <Luis.Felger@de.bosch.com>
2020-04-27 18:46:40 +0200

    Finished the TrainingNodeProviderImpl

--- securityrat-backend/src/main/java/org/appsec/securityrat/provider/mapper/training/TrainingTreeNodeMapper.java
+++ securityrat-backend/src/main/java/org/appsec/securityrat/provider/mapper/training/TrainingTreeNodeMapper.java
@@ -4,8 +4,13 @@ import org.appsec.securityrat.api.dto.training.TrainingTreeNodeDto;
 import org.appsec.securityrat.domain.TrainingTreeNode;
 import org.appsec.securityrat.provider.mapper.IdentifiableMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TrainingTreeNodeMapper
         extends IdentifiableMapper<Long, TrainingTreeNode, TrainingTreeNodeDto> {
+    
+    void updateDto(
+            TrainingTreeNode source,
+            @MappingTarget TrainingTreeNodeDto target);
 }
