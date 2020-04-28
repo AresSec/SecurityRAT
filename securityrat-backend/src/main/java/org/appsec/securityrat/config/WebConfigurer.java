4ffa2eff0da2ae2101763fdcb481bb91dc9d0f69
Luis Felger <Luis.Felger@de.bosch.com>
2020-04-28 11:17:06 +0200

    Fixed GZIP filter issue

--- securityrat-backend/src/main/java/org/appsec/securityrat/config/WebConfigurer.java
+++ securityrat-backend/src/main/java/org/appsec/securityrat/config/WebConfigurer.java
@@ -3,6 +3,9 @@ package org.appsec.securityrat.config;
 import io.github.jhipster.config.JHipsterConstants;
 import io.github.jhipster.config.JHipsterProperties;
 import io.github.jhipster.web.filter.CachingHttpHeadersFilter;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -11,16 +14,13 @@ import javax.inject.Inject;
 import javax.servlet.*;
 import java.util.Arrays;
 import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.appsec.securityrat.web.filter.StaticResourcesProductionFilter;
-import org.appsec.securityrat.web.filter.gzip.GZipServletFilter;
 import org.springframework.boot.web.server.MimeMappings;
 import org.springframework.boot.web.server.WebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.core.env.Profiles;
 
 /**
  * Configuration of web application with Servlet 3.0 APIs.
@@ -40,12 +40,13 @@ public class WebConfigurer implements ServletContextInitializer,
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
+        
         EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
-        if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_PRODUCTION)) {
+        
+        if (env.acceptsProfiles(Profiles.of(JHipsterConstants.SPRING_PROFILE_PRODUCTION))) {
             initCachingHttpHeadersFilter(servletContext, disps);
-            initStaticResourcesProductionFilter(servletContext, disps);
-            initGzipFilter(servletContext, disps);
         }
+        
         log.info("Web application fully configured");
     }
 
@@ -70,45 +71,24 @@ public class WebConfigurer implements ServletContextInitializer,
             mappings.add("json", "text/html;charset=utf-8");
             configurableFactory.setMimeMappings(mappings);
         }
+        
+        // Document root
+        
+        if (configurableFactory != null) {
+            URL indexHtmlUrl = WebConfigurer.class.getResource(
+                    "/webapp/index.html");
+            
+            log.debug("Located index.html at {}", indexHtmlUrl);
+            
+            try {
+                configurableFactory.setDocumentRoot(
+                        new File(indexHtmlUrl.toURI()).getParentFile());
+            } catch (URISyntaxException ex) {
+                log.error("Cannot set document root", ex);
+            }
+        }
     }
     
-    /**
-     * Initializes the GZip filter.
-     */
-    private void initGzipFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
-        log.debug("Registering GZip Filter");
-        FilterRegistration.Dynamic compressingFilter = servletContext.addFilter("gzipFilter", new GZipServletFilter());
-        Map<String, String> parameters = new HashMap<>();
-        compressingFilter.setInitParameters(parameters);
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.css");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.json");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.html");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.js");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.svg");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "*.ttf");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "/api/*");
-        compressingFilter.addMappingForUrlPatterns(disps, true, "/metrics/*");
-        compressingFilter.setAsyncSupported(true);
-    }
-
-    /**
-     * Initializes the static resources production Filter.
-     */
-    private void initStaticResourcesProductionFilter(ServletContext servletContext,
-                                                     EnumSet<DispatcherType> disps) {
-
-        log.debug("Registering static resources production Filter");
-        FilterRegistration.Dynamic staticResourcesProductionFilter =
-                servletContext.addFilter("staticResourcesProductionFilter",
-                        new StaticResourcesProductionFilter());
-
-        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/");
-        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/index.html");
-        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/assets/*");
-        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
-        staticResourcesProductionFilter.setAsyncSupported(true);
-    }
-
     /**
      * Initializes the caching HTTP Headers Filter.
      */
--- securityrat-backend/src/main/java/org/appsec/securityrat/web/filter/gzip/GzipResponseHeadersNotModifiableException.java
+++
@@ -1,9 +0,0 @@
-package org.appsec.securityrat.web.filter.gzip;
-
-import javax.servlet.ServletException;
-
-public class GzipResponseHeadersNotModifiableException extends ServletException {
-    public GzipResponseHeadersNotModifiableException(String message) {
-        super(message);
-    }
-}
