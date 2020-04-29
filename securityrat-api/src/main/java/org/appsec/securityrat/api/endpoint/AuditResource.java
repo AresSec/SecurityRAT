be68a15bbd07104f7292789bccf3b003627720b8
Luis Felger <Luis.Felger@de.bosch.com>
2020-04-29 13:41:30 +0200

    Fixed the audit endpoint

--- securityrat-api/src/main/java/org/appsec/securityrat/api/endpoint/AuditResource.java
+++ securityrat-api/src/main/java/org/appsec/securityrat/api/endpoint/AuditResource.java
@@ -1,6 +1,8 @@
 package org.appsec.securityrat.api.endpoint;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import javax.inject.Inject;
 import org.appsec.securityrat.api.dto.AuditEventDto;
@@ -24,11 +26,15 @@ public class AuditResource {
     
     @GetMapping("/audits/byDates")
     public ResponseEntity<List<AuditEventDto>> findByDates(
-            @RequestParam("fromDate") Instant fromDate,
-            @RequestParam("toDate") Instant toDate) {
+            @RequestParam("fromDate") LocalDate fromDate,
+            @RequestParam("toDate") LocalDate toDate) {
         
-        return ResponseEntity.ok(this.systemInfo.getAuditEvents(
-                fromDate,
-                toDate));
+        Instant from = fromDate.atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+        
+        Instant to = toDate.atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+        
+        return ResponseEntity.ok(this.systemInfo.getAuditEvents(from, to));
     }
 }
